0f29cbb0d867daa78a2e7b9a3928132b997d93d4
Adarshsathyan <adarshsathyan2000@gmail.com>
2021-09-09 11:36:45 +0530

    added javascript works

---
+++ JavaScript_works/script.js
@@ -0,0 +1,17 @@
+// javascript programming
+// variable - to represt memory location
+// var, let, const -> decalre a variable.
+//simply name is identifier
+
+//age name
+//hai i am name age yeasrs old
+var company = "Luminar";
+var location = "Kakkanad"
+console.log(company);
+console.log(location);
+
+var age = 20;
+var name = "Adarsh Sathyan";
+console.log(age,name);
+console.log("Hai i am", name ,age,"years old")
+
