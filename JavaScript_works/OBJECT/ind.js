afedc0d47ab5a040bceba29a5a24a7b3c60de0de
Adarshsathyan <adarshsathyan2000@gmail.com>
2021-10-05 16:27:00 +0530

    done rest counry problems in object

---
+++ JavaScript_works/OBJECT/ind.js
@@ -0,0 +1,172 @@
+var country = [
+    
+        {
+            "name": "British Indian Ocean Territory",
+            "topLevelDomain": [
+                ".io"
+            ],
+            "alpha2Code": "IO",
+            "alpha3Code": "IOT",
+            "callingCodes": [
+                "246"
+            ],
+            "capital": "Diego Garcia",
+            "altSpellings": [
+                "IO"
+            ],
+            "subregion": "Eastern Africa",
+            "region": "Africa",
+            "population": 3000,
+            "latlng": [
+                -6.0,
+                71.5
+            ],
+            "demonym": "Indian",
+            "area": 60.0,
+            "timezones": [
+                "UTC+06:00"
+            ],
+            "nativeName": "British Indian Ocean Territory",
+            "numericCode": "086",
+            "flags": {
+                "svg": "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_the_Commissioner_of_the_British_Indian_Ocean_Territory.svg",
+                "png": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_the_Commissioner_of_the_British_Indian_Ocean_Territory.svg/320px-Flag_of_the_Commissioner_of_the_British_Indian_Ocean_Territory.svg.png"
+            },
+            "currencies": [
+                {
+                    "code": "USD",
+                    "name": "United States dollar",
+                    "symbol": "$"
+                }
+            ],
+            "languages": [
+                {
+                    "iso639_1": "en",
+                    "iso639_2": "eng",
+                    "name": "English",
+                    "nativeName": "English"
+                }
+            ],
+            "translations": {
+                "br": "Território Britânico do Oceano íÍdico",
+                "pt": "Território Britânico do Oceano Índico",
+                "nl": "Britse Gebieden in de Indische Oceaan",
+                "hr": "Britanski Indijskooceanski teritorij",
+                "fa": "قلمرو بریتانیا در اقیانوس هند",
+                "de": "Britisches Territorium im Indischen Ozean",
+                "es": "Territorio Británico del Océano Índico",
+                "fr": "Territoire britannique de l'océan Indien",
+                "ja": "イギリス領インド洋地域",
+                "it": "Territorio britannico dell'oceano indiano",
+                "hu": "Brit Indiai-óceáni Terület"
+            },
+            "flag": "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_the_Commissioner_of_the_British_Indian_Ocean_Territory.svg",
+            "regionalBlocs": [
+                {
+                    "acronym": "AU",
+                    "name": "African Union",
+                    "otherNames": [
+                        "الاتحاد الأفريقي",
+                        "Union africaine",
+                        "União Africana",
+                        "Unión Africana",
+                        "Umoja wa Afrika"
+                    ]
+                }
+            ],
+            "independent": true
+        },
+        {
+            "name": "India",
+            "topLevelDomain": [
+                ".in"
+            ],
+            "alpha2Code": "IN",
+            "alpha3Code": "IND",
+            "callingCodes": [
+                "91"
+            ],
+            "capital": "New Delhi",
+            "altSpellings": [
+                "IN",
+                "Bhārat",
+                "Republic of India",
+                "Bharat Ganrajya"
+            ],
+            "subregion": "Southern Asia",
+            "region": "Asia",
+            "population": 1380004385,
+            "latlng": [
+                20.0,
+                77.0
+            ],
+            "demonym": "Indian",
+            "area": 3287590.0,
+            "gini": 35.7,
+            "timezones": [
+                "UTC+05:30"
+            ],
+            "borders": [
+                "AFG",
+                "BGD",
+                "BTN",
+                "MMR",
+                "CHN",
+                "NPL",
+                "PAK",
+                "LKA"
+            ],
+            "nativeName": "भारत",
+            "numericCode": "356",
+            "flags": {
+                "svg": "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+                "png": "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/320px-Flag_of_India.svg.png"
+            },
+            "currencies": [
+                {
+                    "code": "INR",
+                    "name": "Indian rupee",
+                    "symbol": "₹"
+                }
+            ],
+            "languages": [
+                {
+                    "iso639_1": "hi",
+                    "iso639_2": "hin",
+                    "name": "Hindi",
+                    "nativeName": "हिन्दी"
+                },
+                {
+                    "iso639_1": "en",
+                    "iso639_2": "eng",
+                    "name": "English",
+                    "nativeName": "English"
+                }
+            ],
+            "translations": {
+                "br": "Índia",
+                "pt": "Índia",
+                "nl": "India",
+                "hr": "Indija",
+                "fa": "هند",
+                "de": "Indien",
+                "es": "India",
+                "fr": "Inde",
+                "ja": "インド",
+                "it": "India",
+                "hu": "India"
+            },
+            "flag": "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+            "regionalBlocs": [
+                {
+                    "acronym": "SAARC",
+                    "name": "South Asian Association for Regional Cooperation"
+                }
+            ],
+            "cioc": "IND",
+            "independent": true
+        }
+    
+]
+
+console.log(country[0].currencies[0].symbol);
---
+++ JavaScript_works/OBJECT/transactions.js
@@ -0,0 +1,58 @@
+var accounts = {
+    1001: {
+        acno: 1001, password: "userone", balance: 5000, transactions:
+            [
+                { to: 1002, amount: 500 },
+                { to: 1003, amount: 600 },
+                { to: 1004, amount: 700 }
+            ]
+    },
+    1002: {
+        acno: 1002, password: "usertwo", balance: 7000,
+        transactions:
+            [
+                { to: 1001, amount: 500 },
+                { to: 1003, amount: 600 },
+                { to: 1004, amount: 700 }
+            ]
+    },
+    1003: {
+        acno: 1003, password: "userthree", balance: 8000,
+        transactions:
+            [
+                { to: 1001, amount: 500 },
+                { to: 1002, amount: 600 },
+                { to: 1004, amount: 700 }
+            ]
+    },
+    1004: {
+        acno: 1001, password: "userone", balance: 9000,
+        transactions:
+            [
+                { to: 1001, amount: 500 },
+                { to: 1003, amount: 600 },
+                { to: 1004, amount: 700 }
+            ]
+    },
+
+}
+
+
+
+//function to print transcations
+let transaction = (accno)=>{
+    if(accno in accounts){
+        let transaction_amts = accounts[accno].transactions;
+        console.log("Total transactions",transaction_amts);
+    }else{
+        console.log("Inavlid account number");
+    }
+}
+
+transaction(1001);
+
+
+let data =accounts[1001].transactions.filter(amt => amt.amount>500)
+
+console.log(data);
+
